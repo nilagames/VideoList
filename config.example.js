5fbb0cde71da6836b368932653bc85cc726d96a6
Param-Harrison <paramanantham@live.com>
2017-12-23 23:13:25 +0200

    fix: update config

--- config.example.js
+++ config.example.js
@@ -1,8 +1,11 @@
+import { AdMobBanner } from 'react-native-admob';
+
 const config = {
   YTKey: 'IamTheKeyMan',
   appodealKey: 'IamAppodealKey',
   GACode: 'IamGACodeMan',
   admobBannerID: 'ca-app-pub-3940256099942544/6300978111', // Test ID
+  testIds: [AdMobBanner.simulatorId, 'device-ids']
 };
 
 export default config;

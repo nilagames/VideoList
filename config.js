4607a9007a464b8ab294189cf37423e1ecede2ca
Param-Harrison <paramanantham@live.com>
2017-12-28 15:02:54 +0200

    fix: config js added to master

---
+++ config.js
@@ -0,0 +1,10 @@
+import { AdMobBanner } from 'react-native-admob';
+
+const config = {
+  YTKey: 'AIzaSyD-P5xpeCL1VsXkxavAchV4kAohn_xrXvI',
+  GACode: 'UA-110211252-1',
+  admobBannerID: 'ca-app-pub-2304683814183135/8496654545',
+  testIds: [AdMobBanner.simulatorId, 'DA403528E706B64D87753925316CEF72', '24EBFA1D70DA1A7DE05D7B1FF1EFBE97']
+};
+
+export default config;
